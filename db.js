a89411e07eaaebe2e1e8a44e5c85884de97a9d74
Mark Gauffin <markgauffin@gmail.com>
2022-01-20 04:23:55 -0500

    add database file

---
+++ db.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    // Connect to database
+    mongoose.connect(process.env['MONGO_URI']);
+
+    // Define schemas
+    const { Schema, model } = mongoose;
+
+    const issueSchema = new Schema({
+        issue_title: String,
+        issue_text: String,
+        created_on: Date,
+        updated_on: Date,
+        created_by: String,
+        assigned_to: String,
+        open: Boolean,
+        status_text: String,
+    });
+
+    const projectSchema = new Schema({
+        issues: [issueSchema],
+    });
+
+    // Define models
+    const Project = model('Project', projectSchema);
+    const Issue = model('Issue', issueSchema);
+};
