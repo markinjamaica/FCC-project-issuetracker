2dc884a1bee93afb79c519d384551dcf36cd496f
Mark Gauffin <markgauffin@gmail.com>
2022-01-27 06:37:22 -0500

    fix: add conditional to assigned_to

--- routes/api.js
+++ routes/api.js
@@ -34,7 +34,7 @@ module.exports = function (app, Issue) {
                 issue_title: req.body.issue_title,
                 issue_text: req.body.issue_text,
                 created_by: req.body.created_by,
-                assigned_to: req.body.assigned_to,
+                assigned_to: req.body.assigned_to || '',
                 open: true,
                 status_text: req.body.status_text || '',
                 created_on: date,
